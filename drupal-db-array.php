dcfb63e115c365540dab0e17ea401708951a48c2
Brian McVeigh <bmcveigh@metrostarsystems.com>
2018-05-23 12:49:05 -0400

    Initial commit.

---
+++ drupal-db-array.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * This is scaffolding for configuring a db that can
+ * be found in settings.php. Normally, we would not
+ * want to commit this file but since this just provides
+ * as a simple template for reference, it is okay.
+ */
+
+$databases['default']['default'] = array(
+   'driver' => 'mysql',
+   'database' => 'databasename',
+   'username' => 'username',
+   'password' => 'password',
+   'host' => 'localhost',
+   'prefix' => '',
+);
+
